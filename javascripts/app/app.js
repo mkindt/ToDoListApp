a2dc4f7e5eb22644c11c311535de1a78fe2f7230
mkindt <mkindt@unca.edu>
2013-03-21 23:22:01 -0400

    modified code to pass HTML and JS Lints

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,107 +1,103 @@
-var main = function () {
-  console.log("hello world!");
-  var toDoItems = 6;
-  var tabCheck = 0;
-  var setUpClickHandler = function (anchor) {
-    anchor.click(function () {
-      var target = $(this).attr("href");
-      $("#"+target).css("display", "none");
-      $(".active").removeClass("active");
-      $(this).addClass("active");
-      $("#"+target).addClass("active");
-      //$("#tab2").
-      console.log("hrmm " + target);
-      if (target === "tab2") {
-        categorize();
-        tabCheck = 1;
-      }
-      else {
-        tabCheck = 0;
-      }
-      $("#"+target).fadeIn(1000);
-      return false;
-    });    
-  };
-  var deleteTab1 = new Array;
-  var deleteButton = (function() {
-    $(".deleter").click(function() { 
-      deleteTab1 = $(this).parent().attr("class").split(" ");
-      deleteTab1.splice(0,1);
-      //$(this).parent().parent().remove();
-      $("#tab1").find("." +deleteTab1).parent().hide("scale", function() {
-        $("#tab1").find("." +deleteTab1).parent().remove();
+/*global $, console, window, document, jQuery*/
+(function () {
+  'use strict';
+  var $, $doc, toDoItems, tabCheck, setUpClickHandler, target, deleteTab1, listClasses, newItem, allCategories, foundToDo;
+  $ = window.$;
+
+  $(document).ready(function () {
+    toDoItems = 6;
+    tabCheck = 0;
+    deleteTab1 = [];
+    function deleteButton() {
+      $(".deleter").click(function() {
+        deleteTab1 = $(this).parent().attr("class").split(" ");
+        deleteTab1.splice(0, 1);
+        $("#tab1").find("." + deleteTab1).parent().hide("scale", function() {
+          $("#tab1").find("." + deleteTab1).parent().remove();
+        });
+        console.log("got hre " + deleteTab1);
+        if (tabCheck === 1) {
+          categorize();
+        }
+        return false;
       });
-      console.log("got hre " +deleteTab1);
-      //if (tabCheck === 1) {
-        categorize();
-      //}
-      return false;
-    });     
-  });
-  
-  var submitButton = (function() {
-    $( "#newEdit" )
-      .click(function() {
+    }
+
+    function categorize() {
+      allCategories = [];
+      foundToDo = [];
+      $("#toDo").children().each(function () {
+        var listClasses = $(this).attr("class").split(" ");
+        console.log(listClasses);
+        allCategories = allCategories.concat(listClasses);
+      });
+      allCategories.sort();
+      $.unique(allCategories);
+      console.log("singles? " + allCategories);
+      $("#tab2").empty();
+      $(allCategories).each(function() {
+        var showCat = this.toUpperCase();
+        $("#tab2").append("<h3>" + showCat + "</h3>");
+        $("#toDo").find($("." + this)).clone().appendTo("#tab2");
+      });
+      deleteButton();
+    }
+
+    setUpClickHandler = function (anchor) {
+      anchor.click(function () {
+        target = $(this).attr("href");
+        $("#" + target).css("display", "none");
+        $(".active").removeClass("active");
+        $(this).addClass("active");
+        $("#" + target).addClass("active");
+        console.log("hrmm " + target);
+        if (target === "tab2") {
+          categorize();
+          tabCheck = 1;
+        } else {
+          tabCheck = 0;
+        }
+        $("#" + target).fadeIn(1000);
+        return false;
+      });
+    };
+
+    function submitButton() {
+      $("#newEdit").click(function() {
         console.log($("#newToDo").val());
         console.log($("#newCats").val());
-        var listClasses = $("#newCats").val().split(","); //entering with spaces breaks categorize()
-        var newItem = $("#newToDo").val();
+        listClasses = $("#newCats").val().split(",");
+        newItem = $("#newToDo").val();
         toDoItems = toDoItems + 1;
-        $("#toDo").append("<div class='" + listClasses.join(' ') + "'><div class='left toDoItem" + toDoItems +"'>" + newItem + "<input name='submit' type='image' class = 'deleter' src='images/delete-icon.png'/></div><div class = 'right'><div class = 'cats'>"+ listClasses + "</div></div></div>");
+        $("#toDo").append("<div class='" + listClasses.join(' ') + "'><div class='left toDoItem" + toDoItems + "'>" + newItem + "<input name='submit' type='image' class = 'deleter' src='images/delete-icon.png'/></div><div class = 'right'><div class = 'cats'>" + listClasses + "</div></div></div>");
         $("#tab3").append("<div>'" + newItem + "' added.</div>");
         deleteButton();
         return false;
-      });     
-  });
-  
-  
+      });
+    }
 
+    $.getJSON("json/all.json", function (todos) {
+      todos.forEach(function (todo) {
+        toDoItems = toDoItems + 1;
+        $("#toDo").append("<div class='" + todo.categories.join(' ') + "' style = 'display:none'><div class='left toDoItem" + toDoItems + "'>" + todo.description + "<input name='submit' type='image' class = 'deleter' src='images/delete-icon.png'/></div><div class = 'right'><div class = 'cats'>" + todo.categories + "</div></div></div>");
+        $("#toDo").children().slideDown(1000);
+      });
+      deleteButton();
+    });
 
-  function categorize() {
-    var allCategories = new Array;
-    var foundToDo = new Array;
     $("#toDo").children().each(function () {
-      var listClasses = $(this).attr("class").split(" ");
-      console.log(listClasses);
-      allCategories = allCategories.concat(listClasses);
+      listClasses = $(this).attr("class").split(" ");
+      $(this).css("display", "none");
+      $(this).children().append("<input name='submit' type='image' class = 'deleter' src='images/delete-icon.png'/>");
+      $(this).append("<div class = 'right'><div class = 'cats'>" + listClasses + "</div></div>");
+      $(this).slideDown(1000);
     });
-    //$.unique(allCategories);
-    allCategories.sort();
-    $.unique(allCategories);
-    console.log("singles? " + allCategories);
-    $("#tab2").empty();
-    $(allCategories).each(function() {
-      var showCat = this.toUpperCase();
-      //$("#tab2").css("display", "none");
-      $("#tab2").append("<h3>" + showCat + "</h3>");
-      $("#toDo").find($("." +this)).clone().appendTo("#tab2");
-      //$("#tab2").slideDown(500);
-      deleteButton(); //bad form to call each time?
-    });
-  };
 
-  
-  $.getJSON("json/all.json", function (todos) {
-    todos.forEach(function (todo) {
-      toDoItems = toDoItems + 1;
-      $("#toDo").append("<div class='" + todo.categories.join(' ') + "' style = 'display:none'><div class='left toDoItem" + toDoItems +"'>" + todo.description + "<input name='submit' type='image' class = 'deleter' src='images/delete-icon.png'/></div><div class = 'right'><div class = 'cats'>" + todo.categories + "</div></div></div>");
-      $("#toDo").children().slideDown(1000);
-    });
+    console.log("about to set up click handlers");
+    setUpClickHandler($(".tabs .tab"));
     deleteButton();
-  });
-  
-  $("#toDo").children().each(function () {
-    var listClasses = $(this).attr("class").split(" ");
-    $(this).css("display", "none");
-    $(this).children().append("<input name='submit' type='image' class = 'deleter' src='images/delete-icon.png'/>");
-    $(this).append("<div class = 'right'><div class = 'cats'>" + listClasses + "</div></div>");
-    $(this).slideDown(1000);
-  });
+    submitButton();
 
-  console.log("about to set up click handlers");
-  setUpClickHandler($(".tabs .tab"));
-  deleteButton();
-  submitButton();
-}
-
-$(document).ready(main);
+    $doc = $(document);
+  });
+}(jQuery));
